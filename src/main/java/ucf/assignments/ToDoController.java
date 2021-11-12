2f38084c86ea896711227d65081714a77e35806c
Zimbee <61887028+Zmbeboy@users.noreply.github.com>
2021-11-11 19:59:23 -0500

    Add Item can now add Items!

--- src/main/java/ucf/assignments/ToDoController.java
+++ src/main/java/ucf/assignments/ToDoController.java
@@ -6,6 +6,7 @@ package ucf.assignments;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -25,21 +26,51 @@ public class ToDoController {
     @FXML
     private TableColumn<Item,String> completion;
 
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private DatePicker dateField;
+
+    @FXML
+    private CheckBox completionField;
+
+    @FXML
+    private Button addItem;
+
     ObservableList<Item> list = FXCollections.observableArrayList(
 
-         new Item("IanGavernEYSSSYEYSYEYSYEYSYEYSEYSYEYS","2002-06-03","Complete"),
+         new Item("ItemTest1","2002-06-03","Complete"),
          new Item("ItemTest2","2002-06-04","Complete"),
          new Item("ItemTest3","2002-06-28","Incomplete"),
          new Item("ItemTest4","2002-06-06","Complete")
     );
 
+
     @FXML
     protected void initialize()
     {
+        //list.add();
         description.setCellValueFactory(new PropertyValueFactory<Item,String>("description"));
         date.setCellValueFactory(new PropertyValueFactory<Item,String>("date"));
         completion.setCellValueFactory(new PropertyValueFactory<Item,String>("completion"));
 
         table.setItems(list);
+    }
+
+    public void addItemClick(ActionEvent actionEvent)
+    {
+        String comp;
+        String desc = descriptionField.getText();
+        String date = dateField.getValue().toString();
+        if(completionField.isSelected())
+        {
+            comp = "Complete";
+        }
+        else
+        {
+            comp = "Incomplete";
+        }
+        list.add(new Item(desc,date,comp));
     }
 }
